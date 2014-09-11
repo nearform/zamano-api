0da4e4c322a05c52d88564d341cf546d546328ed
Jake Pruitt <jrpruit1@asu.edu>
2014-09-11 01:27:28 -0700

    Added messageHandler functionality and built HTTP error handling for sendMessage

--- test/zamano-api.test.js
+++ test/zamano-api.test.js
@@ -5,12 +5,4 @@ describe('zamano-api basics', function() {
 	it('should have a version number', function() {
 		assert.equal(zamano.version, '0.0.0');
 	});
-
-	it.skip('should be configurable with different passwords and usernames', function() {
-		// more tests ...
-	});
-
-	it.skip('should throw an error if username and password are not set', function() {
-		// authentication tests ...
-	});
 });
--- test/zamano-send-messages.test.js
+++ test/zamano-send-messages.test.js
@@ -1,25 +1,25 @@
-var assert = require('chai').assert;
-var zamano = require('../')('TEST_CLIENT_ID', 'TEST_PASSWORD');
-var nock   = require('nock');
-var http   = require('http');
+var assert = require('chai').assert,
+    zamano = require('../')('TEST_CLIENT_ID', 'TEST_PASSWORD'),
+    nock   = require('nock'),
+    http   = require('http')
+
+var mtPath = '/Aggregation/servlet/implementation.listeners.http.SendTextMessage'
 
 describe('zamano-api message submitting', function() {
 	var scope = nock('http://mmg.zamano.com')
 		.persist()
-		.get('/')
-		.replyWithFile(200, __dirname + '/testXML/success.xml')
-		.get('/Aggregation/servlet/implementation.listeners.http.SendTextMessage' +
+		.get(mtPath +
 			'?sourceMsisdn=50015' +
 			'&destinationMsisdn=3538703454' +
 			'&messageText=Hello%20world' +
 			'&operatorId=1' +
 			'&clientId=TEST_CLIENT_ID' +
 			'&password=TEST_PASSWORD')
-		.replyWithFile(200, __dirname + '/testXML/invalid-parameters.xml')
-		.get('/Aggregation/servlet/implementation.listeners.http.SendTextMessage' +
+		.replyWithFile(200, __dirname + '/testXML/success.xml')
+		.get(mtPath +
 			'?sourceMsisdn=50015' +
 			'&destinationMsisdn=3538703454' +
-			'&messageText=Hello%20world' +
+			'&messageText=This%20Causes%20An%20Invalid%20Parameters%20Error' +
 			'&operatorId=1' +
 			'&clientId=TEST_CLIENT_ID' +
 			'&password=TEST_PASSWORD')
@@ -37,12 +37,24 @@ describe('zamano-api message submitting', function() {
 			assert.isObject(out, 'The output is an object')
 			assert.deepEqual(out, { 
 				requestId: '12546',
-			  status: '13',
-			  responseID: '545466',
-			  errorText: 'Invalid Parameters' }, 'The output matches the XML document')
+			  status: '0',
+			  responseID: '545466' }, 'The output matches the XML document')
 			done()
 		})
 
+	})
+
+	it('should callback an error when the response has an error', function(done) {
+		zamano.sendMessage({
+			sourceMsisdn:'50015',
+			destinationMsisdn:'3538703454',
+			messageText: 'This Causes An Invalid Parameters Error',
+			operatorId: '1'
+		}, function(err, out) {
+			assert.isNotNull(err, 'Error is not null')
+			assert.throws(function() {throw err}, /Invalid Parameters/)
+			done()
+		})
 	})
 
 	it('should respond with an error if all fields are missing', function(done) {
@@ -53,4 +65,4 @@ describe('zamano-api message submitting', function() {
 		})
 	})
 
-});
+})
