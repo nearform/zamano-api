6e3501724ec64c1ddd82a5c847524ff40c5606e5
Jake Pruitt <jrpruit1@asu.edu>
2014-09-12 09:44:38 -0700

    Updated zamano-api.js with .bind and comments

--- zamano-api.js
+++ zamano-api.js
@@ -4,18 +4,22 @@ var http        = require('http'),
 
 
 function sendMessage (opts, cb) {
-	// Set defaults for opts
+	// Check for missing required fields
 	opts = opts || {}
 	var requiredFields = ['sourceMsisdn','destinationMsisdn','messageText','operatorId']
 	var missingFields = requiredFields.filter(function(key) {
 		return !opts.hasOwnProperty(key)
 	})
 	if(missingFields.length) {
-		return setImmediate(function() {
-			return cb(new Error('Missing required fields: ' + missingFields.join(', ')))
-		})
+		// Return and call the callback with an error asynchronously
+		return setImmediate(
+			cb.bind(null, 
+				new Error('Missing required fields: ' + missingFields.join(', '))
+			)
+		)
 	}
 	
+	// Defaults ID and password to global parameters
 	opts.clientId = opts.clientId || this.zamanoId
 	opts.password = opts.password || this.password
 
@@ -25,20 +29,23 @@ function sendMessage (opts, cb) {
 	http.get('http://mmg.zamano.com' + 
 		'/Aggregation/servlet/implementation.listeners.http.SendTextMessage?' + params, 
 		handleResponse)
-	.on('error', function(err) {
+	.on('error', function handleError(err) {
 		return cb(err)
 	})
 
 	function handleResponse(res) {
 		var xmlResponse = ''
-		res.on('data', function(chunk) {
+		// Collect response into one string
+		res.on('data', function aggregateResponse(chunk) {
 			xmlResponse += chunk
 		})
 		res.on('end', translateXML)
 
 		function translateXML() {
-			parseString(xmlResponse, function(err, result) {
+			// Parse string of XML to object
+			parseString(xmlResponse, function useObject(err, result) {
 				if (err) { return cb(err) }
+				// Only return success if XML says it was successful
 				if (result.status === '0') { return cb(null, result) }
 
 				return cb(new Error(result.errorText))
@@ -51,6 +58,7 @@ function messageHandler (opts) {
 	var self = this
 
 	return function hook(req, res, next) {
+		// Check if the request used the correct ID and password
 		if (req.query.username === self.zamanoId && req.query.password === self.password) {
 			req.mobileMessage = req.query
 			next()
